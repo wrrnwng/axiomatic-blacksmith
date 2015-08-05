ce5a7f0eaf0d4133470f03a806bd2fa991d4dea2
Adnan Pirzada <adpirz@Adnans-MacBook-Pro.local>
2015-08-05 16:20:14 -0700

    Added post routes for /questions and /answer

--- app/routes.js
+++ app/routes.js
@@ -1,13 +1,49 @@
 module.exports = function(app) {
 
-	// server routes ===========================================================
-	// handle things like api calls
-	// authentication routes
+  // server routes ===========================================================
+  // handle things like api calls
+  // authentication routes
 
-	// frontend routes =========================================================
-	// route to handle all angular requests
-	app.get('*', function(req, res) {
-		res.sendfile('./public/index.html');
-	});
+  // frontend routes =========================================================
+  // route to handle all angular requests
+  app.get('*', function(req, res) {
+    res.sendfile('./public/index.html');
+  });
 
+
+  // questions (NOT answers) are posted via '/questions' route
+  app.post('/questions', function(req, res) {
+    var question = new QuestionModel({
+      title: req.body.title,
+      body: req.body.body,
+      student: req.body.student,
+      answeredBy: null,
+      answer: null
+    })
+
+    // save the newly created model to Mongoose
+    question.save(function(err, data){
+      if (err) throw err;
+      console.log("Saved!");
+    })
+  }
+
+  //answers are posted to pre-existing question models
+  app.post('/answer', function(req, res){
+
+    // find the corresponding question by primary key
+    Question.findOne( { _id : req.body.questionid } ).exec(function(err, question){
+      if(err) throw err;
+      question.answeredBy = req.body.teacher;
+      question.answer = req.body.answer;
+      question.save()
+    })
 };
+
+{
+  title: "What is Angular?",
+  body: "etc.",
+  student: "Adnan",
+  answeredBy: undefined,
+  answer: undefined
+}
