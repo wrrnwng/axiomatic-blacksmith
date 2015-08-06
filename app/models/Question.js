83ccbd9fa0e2cc0ccd18e0ae7094b671eefef8ef
Warren Wong <wrrnwng@gmail.com>
2015-08-05 18:57:13 -0700

    resolves #19: added user and video relationships

--- app/models/Question.js
+++ app/models/Question.js
@@ -3,11 +3,11 @@ var mongoose = require('mongoose');
 var questionSchema = mongoose.Schema({
     title: String,
     body: String,
-    student: String,
-    answeredBy: String,
-    answer: String
+    answer: String,
+    student: {type: Schema.Types.ObjectId, ref: 'User'},
+    answeredBy: {type: Schema.Types.ObjectId, ref: 'User'},
+    video: {type: Schema.Types.ObjectId, ref: 'Video'}
 });
 
 var Question = mongoose.model('Question', questionSchema);
-
 module.exports = Question;
