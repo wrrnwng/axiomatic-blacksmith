b63addc21611f8e8bc5a0c309993a012a56b35d6
Daniel Friedenbach <danielfriedenbach@gmail.com>
2015-08-06 19:44:00 -0700

    Implement get request handling for /users route

--- app/routes.js
+++ app/routes.js
@@ -6,6 +6,23 @@ module.exports = function(app) {
 
   // Express routing service for get and post requests
 
+  app.get('/users', function (req, res) {
+    if (req.body.name) {
+      var users = User.findOne({name: name})
+        .exec(function (err, data) {
+          if (err) throw err;
+          res.send(data);
+        });
+    } else {
+      var users = User.find({})
+        .exec(function (err, data) {
+          if (err) throw err;
+          res.send(data);
+        });
+    }
+  });
+
+
   // Returns array of objects;
   // To access name, you'll need to 
   // access item.name (for item in data)
@@ -16,7 +33,7 @@ module.exports = function(app) {
         if(err) throw err;
         res.send(data);
       });
-  })
+  });
 
   // questions (NOT answers) are posted via '/questions' route
   app.post('/questions', function(req, res) {
@@ -26,13 +43,13 @@ module.exports = function(app) {
       student: req.body.student,
       answeredBy: null,
       answer: null
-    })
+    });
 
     // save the newly created model to Mongoose
     question.save(function(err, data){
       if (err) throw err;
       res.send(201);
-    })
+    });
   });
 
   // answers are posted to pre-existing question models;
