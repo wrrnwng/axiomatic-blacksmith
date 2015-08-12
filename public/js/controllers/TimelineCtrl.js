9ad367035941757dc3c5814333c5bf2bc884973b
Warren Wong <wrrnwng@gmail.com>
2015-08-12 16:09:49 -0700

    progress bar experiment

--- public/js/controllers/TimelineCtrl.js
+++ public/js/controllers/TimelineCtrl.js
@@ -1,36 +1,24 @@
 angular.module('TimelineCtrl', [])
 
-.controller('TimelineController', function($scope, VideoFactory, qandaFactory) {
-  var timelineData = [{times: []}];
-  var svg;
-  var chart;
-  qandaFactory.getAnswers()
-    .then(function(questions) {
-      questions.forEach(function(question){
-        timelineData[0].times.push({"starting_time": question.askQTime*1000, "ending_time": question.askQTime*1000+5000})
-      });
-
-      svg = d3.select("#timeline").append("svg").attr('width', '100%')
-        .datum(timelineData).call(chart);
-    });
-
-  function timelineStackedIcons() {
-    chart = d3.timeline()
-      .beginning(0) // we can optionally add beginning and ending times to speed up rendering a little
-      .ending(154000)
-      .tickFormat({
-        format: d3.time.format("%M:%S"),
-        tickTime: d3.time.seconds,
-        tickInterval: 30,
-        tickSize: 10
-      })
-      .margin({left:0, right:15, top:15, bottom:0});   
-    }  
-   timelineStackedIcons();  
+.controller('TimelineController', function($scope, TimelineFactory) {
+  $scope.playPause = function() {
+    
+  };
 })
-  .directive('timeline', function() {
-    return {
-      restrict: 'E',
-      templateUrl: 'views/timeline.html'
+
+.directive('timeline', function() {
+  return {
+    restrict: 'E',
+    templateUrl: 'views/timeline.html'
+  }
+})
+
+.factory('TimelineFactory', function($scope, VideoFactory) {
+
+    if (VideoFactory.isPlaying) {
+      VideoFactory.pause();
+    } else {
+      VideoFactory.play();
     }
-  })
+
+});
